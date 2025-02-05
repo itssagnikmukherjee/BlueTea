bb8e3a0fbb7f490d56c3313f54b29125ab8a5998
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2025-02-05 17:37:15 +0530

    signup done -user

--- app/src/main/java/com/itssagnikmukherjee/blueteauser/presentation/screens/SignUpScreen.kt
+++ app/src/main/java/com/itssagnikmukherjee/blueteauser/presentation/screens/SignUpScreen.kt
@@ -2,6 +2,7 @@ package com.itssagnikmukherjee.blueteauser.presentation.screens
 
 import android.graphics.Color
 import android.graphics.Outline
+import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -16,7 +17,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Colors
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -27,29 +30,31 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil3.Uri
 import coil3.compose.rememberAsyncImagePainter
+import com.itssagnikmukherjee.blueteauser.domain.models.UserData
 import com.itssagnikmukherjee.blueteauser.presentation.ViewModels
 
-@Preview(showBackground = true)
+
 @Composable
 fun SignUpScreen(viewModel: ViewModels = hiltViewModel()) {
     val registrationState = viewModel.registerUserState.collectAsState()
     val context = LocalContext.current
-    when{
-       registrationState.value.isLoading->{
-           CircularProgressIndicator()
-       }
-       registrationState.value.data !=null ->{
-           Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
-       }
-       registrationState.value.error.isNotBlank()->{
-           Toast.makeText(context, registrationState.value.error, Toast.LENGTH_SHORT).show()
-       }
+
+    when {
+        registrationState.value.isLoading -> {
+            CircularProgressIndicator()
+        }
+        registrationState.value.data != null -> {
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+        }
+        registrationState.value.error.isNotBlank() -> {
+            Toast.makeText(context, registrationState.value.error, Toast.LENGTH_SHORT).show()
+        }
     }
 
     var userFirstName by remember { mutableStateOf("") }
@@ -62,34 +67,51 @@ fun SignUpScreen(viewModel: ViewModels = hiltViewModel()) {
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
-    ){uri:android.net.Uri?->
-        userImage = uri as Uri?
+    ) { uri: Uri? ->
+        userImage = uri
     }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ){
-        Box(modifier = Modifier.size(200.dp).background(androidx.compose.ui.graphics.Color.LightGray,
-            CircleShape).clickable{
-                launcher.launch("image/*")
-        }){
-            Image(
-                painter = rememberAsyncImagePainter(userImage),
-                contentDescription = "User Image"
+    ) {
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape).background(color = androidx.compose.ui.graphics.Color.Gray)
+                .clickable {
+                    launcher.launch("image/*")
+                }
+        ) {
+            userImage?.let { uri ->
+                Image(
+                    painter = rememberAsyncImagePainter(uri),
+                    contentDescription = "User Image"
+                )
+            }
+        }
+        Row {
+            OutlinedTextField(value = userFirstName, onValueChange = { userFirstName = it }, placeholder = { Text("First Name") })
+            OutlinedTextField(value = userLastName, onValueChange = { userLastName = it }, placeholder = { Text("Last Name") })
+        }
+        OutlinedTextField(value = userPhoneNo, onValueChange = { userPhoneNo = it }, placeholder = { Text("Phone No") })
+        OutlinedTextField(value = userEmail, onValueChange = { userEmail = it }, placeholder = { Text("Email") })
+        OutlinedTextField(value = userPassword, onValueChange = { userPassword = it }, placeholder = { Text("Password") })
+        OutlinedTextField(value = userAddress, onValueChange = { userAddress = it }, placeholder = { Text("Address") })
+
+        Button(onClick = {
+            val userData = UserData(
+                firstName = userFirstName,
+                lastName = userLastName,
+                phoneNo = userPhoneNo,
+                email = userEmail,
+                password = userPassword,
+                address = userAddress
             )
-
+            viewModel.registerUserWithEmail(userData)
+        }) {
+            Text("Register")
         }
-        Row{
-            OutlinedTextField(value = userFirstName, onValueChange = {userFirstName=it}, placeholder = {Text("First Name")})
-            OutlinedTextField(value = userLastName, onValueChange = {userLastName=it}, placeholder = {Text("Last Name")})
-        }
-        OutlinedTextField(value = userPhoneNo, onValueChange = {userPhoneNo=it}, placeholder = {Text("Phone No")})
-        OutlinedTextField(value = userEmail, onValueChange = {userEmail=it}, placeholder = {Text("Email")})
-        OutlinedTextField(value = userPassword, onValueChange = {userPassword=it}, placeholder = {Text("Password")})
-        OutlinedTextField(value = userAddress, onValueChange = {userAddress=it}, placeholder = {Text("Address")})
-
     }
-
 }
