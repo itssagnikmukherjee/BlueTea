0d58fea5550c31b51e2d39c793c4c88a473a5ad2
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2025-02-05 21:02:17 +0530

    navigation admin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,9 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
     implementation("io.ktor:ktor-client-android:3.0.3")
 
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
